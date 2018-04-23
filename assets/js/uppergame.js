68264573c4d70d520595aa290e79b467378cc16c
Steve-VE <steve.vanessche@gmail.com>
2018-04-23 16:59:42 +0200

    Avancement sur le gameplay

--- assets/js/uppergame.js
+++ assets/js/uppergame.js
@@ -21,24 +21,37 @@ let uppergame = {
         this.state = 0;
 
         this.nextState = () => { // Fonction à appeller lorsque l'on veut passer à la prochaine étape
-            if(this.state == 0){
+            if(this.state == 0){ // Phase de pioche
                 if(this.player.needToDraw() == false && this.opponent.needToDraw() == false){
                     this.state++;
                 }
             }
-            else if(this.state == 1){
-                
+            else if(this.state == 1){ // Phase de sélection des cartes
+                // if(this.player.selectedCard.length > 1 && this.opponent.selectedCard.length > 1){
+                if(this.player.selectedCard.length > 1){
+                    this.state++;
+                }
             }
-            else if(this.state == 2){
+            else if(this.state == 2){ // Traitement des infos
+                this.state++;
+            }
+            else if(this.state == 3){ // On exécute les animations + calcul des dégâts
+                this.opponent.lifepoint -= this.player.attack.strength;
+                this.player.lifepoint -= this.opponent.attack.strength;
 
+                console.log(this.player.attack);
+                console.log(this.opponent.lifepoint);
+                this.state++;
             }
-            else if(this.state == 3){
 
+            if(this.state > 3){
+                this.state = 0;
             }
+            console.log("- ETAPE : " + this.state);
         };
     },
     create: function create () {
-        backgroundArena = this.add.image(400,130, "backgroundArena");
+        backgroundArena = this.add.image(400, 130, "backgroundArena");
         backgroundArena.scaleX = 0.45;
         backgroundArena.scaleY = 0.35;
 
@@ -48,42 +61,27 @@ let uppergame = {
 
         //Référence à la scene actuel, pour pouvoir la cibler là où ce n'est pas possible à l'aide d'un this
         parent = this;
-        lifebarBackPlayer = this.add.sprite(screenWidth(0.10), screenHeight(0.415), 'lifebar');
-        lifebarBackPlayer.anims.play("lifebar-backLife");
-        lifebarBackIA = this.add.sprite(screenWidth(0.90), screenHeight(0.415), 'lifebar');
-        lifebarBackIA.anims.play("lifebar-backLife");
-        lifebarPlayer = this.add.sprite(screenWidth(0.10), screenHeight(0.415), 'lifebar');
-        lifebarPlayer.anims.play("lifebar-lifeStatus");
-        lifebarIA = this.add.sprite(screenWidth(0.90), screenHeight(0.415), 'lifebar');
-        lifebarIA.anims.play("lifebar-lifeStatus");
-
-        lifebarPlayer.displayOriginX = 0;
-        lifebarIA.displayOriginX = lifebarIA.displayWidth;
-        lifebarBackIA.displayOriginX = lifebarBackIA.displayWidth;
-        lifebarBackPlayer.displayOriginX = 0;
+     
         
         confirmButton = this.add.sprite(screenWidth(0.45), screenHeight(0.50), 'thanatalys').setInteractive();
         confirmButton.anims.play('thanatalys-damage');
         confirmButton.name = "confirmButton";
+        confirmButton.setTint("0xaaaaaa");
+
         this.input.on('gameobjectdown', function (pointer, gameObject){
             if(gameObject.name == "confirmButton"){
                 if(parent.state == 1) {
-                    console.log("bla");
                     parent.nextState();
                 }
             }
         });
 
-        //HealthBar
-        // playerCharacter.lifePoints;
-        // IACharacter.lifePoints;
-        // playerCharacter.lifePoints = remainingPV;
         // Gestion camera (TEST)
         this.camera = this.cameras.main.setSize(400, 300);
     },
     update: function update () {
-        lifebarIA.scaleX = (IACharacter.lifePoints * 0.01);
-        lifebarPlayer.scaleX = (selectedCharacter.lifePoints * 0.01);
+        // lifebarIA.scaleX = (IACharacter.lifePoints * 0.01);
+        // lifebarPlayer.scaleX = (selectedCharacter.lifePoints * 0.01);
 
         if(this.state == 0){ // Pioche
             if(this.player.needToDraw()){
@@ -93,12 +91,24 @@ let uppergame = {
                 this.nextState();
             }
 
-            if(this.opponent.length < 7){
-                this.opponent.draw(this);
+            if(this.opponent.needToDraw()){
+                this.opponent.draw(this, false);
             }
         }
+        else if(this.state == 1){ // Sélection des cartes
 
-        this.player.update();
-        this.opponent.update();
+        }
+        else if(this.state == 2){ // Traitement des infos
+            this.player.prepareAttack();
+            this.opponent.prepareAttack();
+            this.nextState();
+        }
+        else if(this.state == 3){ // Animation + dégats
+            
+            this.nextState();
+        }
+
+        this.player.update(this);
+        this.opponent.update(this);
     }
 };
